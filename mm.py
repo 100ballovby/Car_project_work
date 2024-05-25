ca46b4693a8915d8a00b8bca8e9600c97ec2abeb
GreatRaksin <greatraksin@icloud.com>
2024-05-25 18:46:21 +0300

    .

--- mm.py
+++ mm.py
@@ -10,19 +10,80 @@ rect = car.get_rect(center=(W//2, H//2))
 sc.blit(car, rect)
 pygame.display.update()
 
+motion = 'STOP'
+
 car1 = car # делаем копию, чтобы не портить исходник
+angle = 0
 while 1:
+    motion = "STOP"
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             exit()
         elif i.type == pygame.KEYDOWN:
-            pass
+            if i.key == pygame.K_LEFT:
+                angle = 90
+                car = pygame.transform.rotate(car1, angle)
+                rect.x -= 5
+            elif i.key == pygame.K_RIGHT:
+                angle = -90
+                car = pygame.transform.rotate(car1, angle)
+                rect.x += 5
+            elif i.key == pygame.K_UP:
+                angle = 0
+                car = pygame.transform.rotate(car1, angle)
+                rect.y -= 5
+            elif i.key == pygame.K_DOWN:
+                angle = 180
+                car = pygame.transform.rotate(car1, angle)
+                rect.y += 5
             # поворот машины в зависимости от нажатия на кнопку
+
+
     keys = pygame.key.get_pressed()
     # смещение машины (ехать)
+    if keys[pygame.K_a]:
+        angle = 90
+        car = pygame.transform.rotate(car1, angle)
+        rect.x -= 5
+        motion = 'LEFT'
+    elif keys[pygame.K_d]:
+        angle = -90
+        car = pygame.transform.rotate(car1, angle)
+        rect.x += 5
+        motion = 'RIGHT'
+    elif keys[pygame.K_w]:
+        angle = 0
+        car = pygame.transform.rotate(car1, angle)
+        rect.y -= 5
+        motion = 'UP'
+    elif keys[pygame.K_s]:
+        angle = 180
+        car = pygame.transform.rotate(car1, angle)
+        rect.y += 5
+        motion = "DOWN"
+
+    #rect = car.get_rect()
 
     sc.fill(WHITE)
-    sc.blit(car1, rect)
+    sc.blit(car, rect)
+
+    top_x, top_y = rect.topleft
+    left_x, left_y = rect.topleft
+    right_x, right_y = rect.topright
+
+    pygame.draw.line(sc, (255, 0, 0), (top_x, top_y), (top_x + rect.width, top_y), 2)
+    pygame.draw.line(sc, (0, 255, 0), (left_x, left_y), (left_x, left_y + rect.height), 2)
+    pygame.draw.line(sc, (0, 0, 255), (right_x, right_y), (right_x, right_y + rect.height), 2)
+
     pygame.display.update()
 
     pygame.time.delay(20)
+
+    if rect.top <= 0 and motion == 'UP':
+        rect.top = 0
+    elif rect.left <= 0 and motion == 'LEFT':
+        rect.left = 0
+    elif rect.bottom >= H and motion == 'DOWN':
+        rect.bottom = H
+    elif rect.right >= W and motion == 'RIGHT':
+        rect.right = W
